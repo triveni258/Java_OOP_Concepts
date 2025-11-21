1de714b735ea02c2c06ce5ad611a5697cc196e49
triveni258 <triveni.bhaskar999@gmail.com>
2025-11-21 22:58:03 +0530

    AllJavaFiles

---
+++ src/main/java/org/JavaProgram/ReverseOnlyLetters.java
@@ -0,0 +1,23 @@
+package rahulShettyAcademy;
+
+public class ReverseOnlyLetters {
+    public static void main(String[] args) {
+        String inut="1ab2";
+        char inutArray[]=inut.toCharArray();
+        int left=0;
+        int right=inutArray.length-1;
+
+        while(left>right)
+        {
+           // if(!Character.isLetter(inutArray()))
+            {
+                left++;
+            }
+            else if(!Character.isLetter(right))
+            {
+                right--;
+            }
+        }
+        System.out.println();
+    }
+}
