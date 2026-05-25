e407ebcd8059987043a0784e6915d336acb3cc4d
Triveni <swapnil.bodade88@gmail.com>
2026-05-25 12:43:19 +0530

    Upadted FrameworkCode

--- src/main/java/SeleniumConcept/FrameHandle.java
+++ src/main/java/SeleniumConcept/FrameHandle.java
@@ -1,4 +1,16 @@
 package SeleniumConcept;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FrameHandle {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jqueryui.com/droppable/");
+        driver.manage().window().maximize();
+        String title=driver.getTitle();
+        System.out.println(title);
+
+    }
+
 }
